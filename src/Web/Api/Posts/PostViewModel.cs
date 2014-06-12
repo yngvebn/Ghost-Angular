ef20394b289e178b01a3e7e2ce66950e8d8a62f5
Yngve Bakken Nilsen <yngve.nilsen@gmail.com>
2014-06-12 19:46:51 +0200

    Some model stuff

--- src/Web/Api/Posts/PostViewModel.cs
+++ src/Web/Api/Posts/PostViewModel.cs
@@ -1,7 +1,15 @@
-﻿namespace Web.Api.Posts
+﻿using System;
+
+namespace Web.Api.Posts
 {
     public class PostViewModel
     {
-         
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Published { get; set; }
+        public DateTime Updated { get; set; }
+        public string Slug { get; set; }
+        
     }
 }
